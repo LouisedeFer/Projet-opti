7a2a7f304eff63196045479a936a3c6bf56aa01b
Josephine-gith <josephine.peronne@gmail.com>
2025-03-26 17:41:15 +0100

    Q4

--- josephine.py
+++ josephine.py
@@ -1,5 +1,32 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-img = plt.imread('robot_noise.jpg')
-plt.imshow(img)
+imgR0 = plt.imread("robot_no_noise.jpg")
+imgR = imgR0[:, :, 0] / 255
+
+# plt.imshow(imgR)
+
+
+def grad(u):
+    n, m = u.shape[0], u.shape[1]
+
+    dux = u[1:, :] - u[:-1, :]
+    dux = np.concatenate((dux, np.zeros((1, m))), axis=0)
+    duy = u[:, 1:] - u[:, :-1]
+    duy = np.concatenate((duy, np.zeros((n, 1))), axis=1)
+
+    return np.array([dux, duy])
+
+
+def div(v):
+    divx = v[0, 1:, :] - v[0, :-1, :]
+    divx = np.concatenate((divx, np.array([-v[0, -1, :]])), axis=0)
+    divy = v[1, :, 1:] - v[1, :, :-1]
+    divy = np.concatenate((divy, np.array(-v[1, :, -1].reshape(-1, 1))), axis=1)
+    return divx + divy
+
+def laplacien(u):
+    return div(grad(u))
+
+plt.imshow(laplacien(imgR))
 plt.show()
