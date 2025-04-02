9794b44bc18d55559886bb2b3676256fc346723b
Josephine-gith <josephine.peronne@gmail.com>
2025-04-02 17:02:53 +0200

    laplacien à vérifier et Q7 à finir

--- josephine.py
+++ josephine.py
@@ -5,10 +5,10 @@ from scipy import optimize
 imgR0 = plt.imread("robot_no_noise.jpg")
 imgR = imgR0[:, :, 0] / 255
 imgRB0 = plt.imread("robot_noise.jpg")
-imgRB = imgRB0[:,:,0]/255
+imgRB = imgRB0[:, :, 0] / 255
 
 '''
-fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+fig, axes = plt.subplots(1, 4, figsize=(15, 5))
 axes[0].imshow(imgR,cmap='gray')
 axes[0].set_title("Image Originale")
 axes[1].imshow(imgRB,cmap='gray')
@@ -40,45 +40,68 @@ def laplacien(u):
     return div(grad(u))
 
 
-'''
+"""
 axes[1].imshow(laplacien(imgR),cmap='gray')
 axes[1].set_title("Laplacien")
-'''
+"""
 
 ## Q5 Méthode de descente de gradient à pas fixe
 
 
-def optim_gradient_fixed_step(img, L, max_iter=1000, epsilon_grad_fun=1e-8):
+def funR(u, ub):
+    g = grad(u)
+    return (np.linalg.norm(u - ub) ** 2)/2 + np.linalg.norm((g[0] ** 2 + g[1] ** 2)**0.5)
+
+
+def grad_funR(u, ub):
+    return u - ub - 2 * laplacien(u)
+
+
+def approx_grad_funR(x, xb, eps, h):
+    return (funR(x + eps * h, xb) - funR(x - eps * h, xb)) / (2 * eps)
+
+u = np.random.random(imgRB.shape)
+eps = 10 ** (-5)
+h = np.ones(imgRB.shape)
+print(approx_grad_funR(u,imgRB,eps,h)-np.dot(np.transpose(grad_funR(u, imgRB)),h))
+
+def optim_gradient_fixed_step(grad_fun, x0, L, xb, max_iter=100, epsilon_grad_fun=1e-8):
     nb_iter = 0
-    x = img
-    while nb_iter <= max_iter and np.linalg.norm(grad(x)) > epsilon_grad_fun:
+    x = x0
+    while nb_iter <= max_iter and np.linalg.norm(grad_fun(x,xb)) > epsilon_grad_fun:
         nb_iter += 1
-        x -= L * (grad(x)[0] + grad(x)[1])
+        x -= L * grad_fun(x,xb)
     return x
 
 
-imgRB_opti=optim_gradient_fixed_step(imgRB, 1e-5)
+x0, pas = np.zeros(imgR.shape), 1e-3
+imgRB_opti = optim_gradient_fixed_step(grad_funR, x0, pas, imgRB)
 '''
 axes[2].imshow(imgRB_opti,cmap='gray')
 axes[2].set_title("Image après descente de gradient")
-
-plt.show()
 '''
 
 ## Q6 erreur RMSE
 
-def RMSE(u,uVT):
-    n,m=u.shape
-    return np.linalg.norm(uVT-u)/(n*m)**0.5
 
-print(f"RMSE de l'image trouvée par minimisation = {RMSE(imgRB_opti, imgR)}, et RMSE de l'image bruitée = {RMSE(imgRB, imgR)}")
+def RMSE(u, uVT):
+    n, m = u.shape
+    return np.linalg.norm(uVT - u) / (n * m) ** 0.5
+
+
+print(
+    f"RMSE de l'image trouvée par minimisation = {RMSE(imgRB_opti, imgR)}, et RMSE de l'image bruitée = {RMSE(imgRB, imgR)}"
+)
 
 ## Q7 comparaison avec méthode scipy
 
-def fun_img(img):
-    pass
 
-scipy_opti = optimize.minimize(fun_img,imgRB, method="BFGS").x
+def scipy_opti(fun, grad_fun, x0):
+    sol = optimize.minimize(fun, x0.flatten(), jac = grad_fun, method="BFGS").x
+    return sol.reshape(x0.shape)
 
-plt.imshow(scipy_opti)
+'''
+axes[3].imshow(scipy_opti(lambda u : funR(u,imgRB), lambda u : grad_funR(u,imgRB), x0), cmap='gray')
+axes[3].set_title("Image après min par scipy")
 plt.show()
+'''
